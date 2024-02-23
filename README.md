dd27ec668f6d4922d8b573c7b1584123b4c814db
Giang Huynh <trgianghuynhdev1808@gmail.com>
2024-02-23 23:10:01 +0700

    fix: issue not load node environment

--- README.md
+++ README.md
@@ -1,22 +1,28 @@
 ## Requirements
+
 Install packages:
+
+- NodeJS
 - CSpell Ref:
   https://cspell.org/docs/getting-started/
-  
-  ```npm install -g cspell@latest```
+
+  `npm install -g cspell@latest`
 
 - JQ Ref:
   https://www.npmjs.com/package/jq
 
   For Debian/Ubuntu:
-  ```sudo apt-get install jq```  
+  `sudo apt-get install jq`  
   For macOS:
-  ```brew install jq```
+  `brew install jq`
+
 ## Description
-* Bash shell to final checking after coding done
+
+- Bash shell to final checking after coding done
+
 ## Usage
+
 1. Clone repo.
 2. Set `exc` permission for bash file `chmod +x bash.sh`.
 3. Config `script_checking` in `script_check/config.json` file.
-4. Run `bash` file with args `./bash.sh backend` (arg is `script` key in `script_check/config.json` Ex: `backend`, `frontend`,...).
-
+4. Run `bash` file with args `./bash.sh destination/path backend` (arg is `script` key in `script_check/config.json` Ex: `backend`, `frontend`,...).
--- bash.sh
+++ bash.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # args
-source_type=$1
+destination_path=$1
+source_type=$2
 
 # configs
-runner_path="../final-check-bash"
+runner_folder="final-check-bash"
 compare_branch="develop"
 
 echo "*******Final Checking*******"
@@ -10,31 +13,72 @@ echo "*******Final Checking*******"
 #### phase 1: check spelling
 echo "-------Step 1: Check Spelling-------"
 
+# create temp dir to clone content
+mkdir temp
+
+# cd to destination source
+cd $destination_path
+
 # get git changed files
-changed_files=$(git diff --name-only $compare_branch)
+changed_files=$(git diff --diff-filter=AM --name-only $compare_branch)
+
+# clone source to temp
+temp_path="../$runner_folder/temp"
+for file in $changed_files; do
+	dir_name=$(dirname $file)
+
+	if [[ $dir_name == "." ]]; then
+		cp $file $temp_path
+	else
+		temp_dir_path=$temp_path/$dir_name
+		test -d $temp_dir_path
+		exists_folder=$(echo $?)
+
+		# if not exists folder will create it
+		if [[ exists_folder -eq 1 ]]; then
+			mkdir -p $temp_dir_path
+		fi
+
+		cp $file $temp_path/$file
+	fi
+done
+
+# cd to runner temp
+cd ../$runner_folder
 
 # run cspell check for changed files
-report_file_path="$runner_path/out/issue_report.txt"
-config_file_path="$runner_path/cspell/config.json"
+report_file_path="./out/issue_report.txt"
+config_file_path="./cspell/config.json"
 
-cspell_out=$(cspell --config $config_file_path --reporter @cspell/cspell-json-reporter $changed_files)
+cspell_out=$(cspell --config $config_file_path --reporter @cspell/cspell-json-reporter temp/**)
 
 # write to report json file
 echo "$cspell_out" >$report_file_path
 
 # run js summary
-summary_path="$runner_path/cspell/summary-report.js"
+summary_path="./cspell/summary-report.js"
 node $summary_path
+
+# remove temp folder:
+rm -rf temp
 
 # show details info
 echo "Issue report: 'out/issues_report.txt'"
 
 #### phase 2: check based on script_check
+# check args exits
+if [[ -z "$source_type" ]]; then
+	exit 1
+fi
+
 echo "-------Step 2: Check Based On Script-------"
-script_file_path="$runner_path/script_check/config.json"
+script_file_path="./script_check/config.json"
 
 # read script
 script=$(jq -r ".$source_type" "$script_file_path")
+
+# cd destination source
+cd $destination_path
 
 # exc cmd
 eval $script
